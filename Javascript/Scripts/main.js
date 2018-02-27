572f7570729f5fcee8645131099d89a7038d891f
vlino2015 <Veronicaveronica@digitalcrafts.com>
2018-02-27 08:20:50 -0600

    presentation

--- Javascript/Scripts/main.js
+++ Javascript/Scripts/main.js
@@ -1,1 +1,13 @@
-console.log('Hello World!');
+function printSquare(size)
+{
+    var myArrray = [];
+    for(var i=0; i<=size; i++)
+    {
+        myArrray[i] = 
+        
+        console.log(myArray + i);
+    }
+}
+
+printSquare(6);
+
